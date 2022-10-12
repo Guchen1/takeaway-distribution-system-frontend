ddd5a6125dc2e2a55fd0470e67abf0bfda9247b1
guchen <1023797278@qq.com>
2022-10-12 23:56:52 +0800

    overlook finish

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,123 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <n-layout :native-scrollbar="false">
+    <n-layout-header bordered class="underline head" style="display: flex">
+      <div
+        class="line title"
+        :style="{
+          'font-size':
+            width >= 768 ? 'large' : width > 360 ? 'medium' : 'small',
+        }"
+      >
+        {{ width > 512 ? "超级炫酷的外卖分配系统" : "外卖分配系统" }}
+      </div>
+      <n-menu
+        class="line menu"
+        v-model:value="activeKey"
+        mode="horizontal"
+        :options="menuOptions"
+    /></n-layout-header>
+    <n-layout-content
+      :style="{ height: height - 60 - 25 + 'px' }"
+      :native-scrollbar="false"
+      content-style="padding: 24px;"
+    >
+      <RouterView />
+    </n-layout-content>
+    <n-layout-footer>Copyright © 2022 </n-layout-footer>
+  </n-layout>
 </template>
+<script setup>
+import { h, ref } from "vue";
+import { RouterLink } from "vue-router";
+const activeKey = ref("");
+let width = ref(document.documentElement.clientWidth);
+let height = ref(document.body.offsetHeight);
+window.addEventListener("resize", () => {
+  width.value = document.documentElement.clientWidth;
+  height.value = document.body.offsetHeight;
+});
 
+const menuOptions = [
+  {
+    label: () =>
+      h(
+        RouterLink,
+        {
+          to: {
+            name: "home",
+          },
+          style: {
+            "line-height": "60px",
+          },
+        },
+        { default: () => "主页" }
+      ),
+    key: "home",
+  },
+  {
+    label: () =>
+      h(
+        RouterLink,
+        {
+          to: {
+            name: "journal",
+          },
+          style: {
+            "line-height": "60px",
+          },
+        },
+        { default: () => "日志" }
+      ),
+    key: "journal",
+  },
+  {
+    label: () =>
+      h(
+        RouterLink,
+        {
+          to: {
+            name: "settings",
+          },
+          style: {
+            "line-height": "60px",
+          },
+        },
+        { default: () => "设置" }
+      ),
+    key: "settings",
+  },
+];
+</script>
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.line {
+  display: inline-flex;
+  align-items: center;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.title {
+  height: 58px;
+  line-height: 58px;
+  font-weight: bold;
+  font-size: v-bind("small");
+  padding-left: 7.5%;
 }
-
-nav {
+.menu {
+  margin: auto;
+  margin-right: 7.5%;
+}
+.head {
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.underline {
+  border-bottom-width: 2px !important;
 }
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.n-layout-footer {
+  display: flex;
+  justify-content: center;
 }
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+</style>
+<style>
+.n-menu-item {
+  line-height: 60px;
+  height: 60px !important;
 }
 </style>
